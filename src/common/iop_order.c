a6a15d9650187800822023c0d3422eb9cb04b0ec
Kofa <kofa@kovacs-telekes.org>
2025-03-08 11:13:45 +0100

    A rough sketch of Blender's AgX

--- src/common/iop_order.c
+++ src/common/iop_order.c
@@ -139,6 +139,7 @@ const dt_iop_order_entry_t legacy_order[] = {
   { {44.0f }, "lowlight", 0},
   { {45.0f }, "monochrome", 0},
   { {45.3f }, "sigmoid", 0},
+  { {45.5f }, "agx", 0},
   { {46.0f }, "filmic", 0},
   { {46.5f }, "filmicrgb", 0},
   { {47.0f }, "colisa", 0},
@@ -251,6 +252,7 @@ const dt_iop_order_entry_t v30_order[] = {
                                   //    on camera JPEG default look
   { {45.0f }, "filmic", 0},          // same, but different (parametric) approach
   { {45.3f }, "sigmoid", 0},
+  { {45.5f }, "agx", 0},
   { {46.0f }, "filmicrgb", 0},       // same, upgraded
   { {36.0f }, "lut3d", 0},           // apply a creative style or film emulation, possibly non-linear
   { {47.0f }, "colisa", 0},          // edit contrast while damaging colour
@@ -367,6 +369,7 @@ const dt_iop_order_entry_t v50_order[] = {
                                   //    on camera JPEG default look
   { {45.0f }, "filmic", 0},          // same, but different (parametric) approach
   { {45.3f }, "sigmoid", 0},
+  { {45.5f }, "agx", 0},
   { {46.0f }, "filmicrgb", 0},       // same, upgraded
   { {36.0f }, "lut3d", 0},           // apply a creative style or film emulation, possibly non-linear
   { {47.0f }, "colisa", 0},          // edit contrast while damaging colour
@@ -483,6 +486,7 @@ const dt_iop_order_entry_t v30_jpg_order[] = {
   { { 44.0f }, "basecurve", 0 },     // conversion from scene-referred to display referred, reverse-engineered
                                      //    on camera JPEG default look
   { { 45.0f }, "filmic", 0 },        // same, but different (parametric) approach
+  { {45.5f }, "agx", 0},
   { { 45.3f }, "sigmoid", 0},
   { { 46.0f }, "filmicrgb", 0 },     // same, upgraded
   { { 36.0f }, "lut3d", 0 },         // apply a creative style or film emulation, possibly non-linear
@@ -603,6 +607,7 @@ const dt_iop_order_entry_t v50_jpg_order[] = {
                                      //    on camera JPEG default look
   { { 45.0f }, "filmic", 0 },        // same, but different (parametric) approach
   { { 45.3f }, "sigmoid", 0},
+  { {45.5f }, "agx", 0},
   { { 46.0f }, "filmicrgb", 0 },     // same, upgraded
   { { 36.0f }, "lut3d", 0 },         // apply a creative style or film emulation, possibly non-linear
   { { 47.0f }, "colisa", 0 },        // edit contrast while damaging colour
@@ -1172,6 +1177,7 @@ GList *dt_ioppr_get_iop_order_list(const dt_imgid_t imgid,
           _insert_before(iop_order_list, "colorbalance", "diffuse");
           _insert_before(iop_order_list, "nlmeans", "blurs");
           _insert_before(iop_order_list, "filmicrgb", "sigmoid");
+          _insert_before(iop_order_list, "filmicrgb", "agx");
           _insert_before(iop_order_list, "colorbalancergb", "colorequal");
         }
       }
